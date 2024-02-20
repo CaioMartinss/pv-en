85d3f355f9a7196115a1212c698f1c7f47b4dc0c
Caio Martins do Nascimento <martinscaio29@gmail.com>
2024-02-20 20:17:41 -0300

    add: some trash

---
+++ search.js
@@ -0,0 +1,17 @@
+function searchPhrsalVerbs(data, searchTerm) {
+    const searchTermLower = searchTerm.toLowerCase();
+    return (data || []).filter((item) => {
+        // Verifique se item é um objeto antes de acessar suas propriedades
+        if (typeof item === 'object' && item !== null) {
+            return (
+                (item.phrasalVerb || '').toLowerCase().includes(searchTermLower) ||
+                (item.meaning || '').toLowerCase().includes(searchTermLower) ||
+                (item.example || '').toLowerCase().includes(searchTermLower)
+            );
+        }
+        return false;
+    });
+}
+
+
+export default searchPhrsalVerbs;
